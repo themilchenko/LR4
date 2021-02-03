63535df3f4412169c6f23ff797305787695ebb8f
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-02-03 14:24:31 +0300

    Update lr4.cpp

--- lr4.cpp
+++ lr4.cpp
@@ -26,10 +26,22 @@ void bubble_sort(std::vector <int>& a)
 
 void insertion_sort(std::vector <int>& a)
 {
-    for (int i = 0; i < a.size() - 1; i++)
-        for (int j = i; j >= 0; j--)
-            if (a[j + 1] < a[j])
-                swap(a[j + 1], a[j]);
+    for (int i = 1; i < a.size(); i++)
+    {
+        int current = a[i];         /*создаю временную переменную, чтобы запомнить переменную для вставки (если она будет)*/
+        int j = i - 1;              /*создаю индекс для сранения "вставочного" элемента с предыдущими значениями*/
+
+        while (a[j] > current)      /*пока предыдущий элемент больше вставочного*/
+        {
+            a[j + 1] = a[j];        /*сдвигаю элементы вправо для корректной вставки*/
+            j--;
+
+            if (j == -1)            /*проверяю, не вышел ли я из цикла*/
+                break;
+        }
+
+        a[j + 1] = current;         /*вставляю элемент на нужную позицию*/
+    }
 }
 
 void merge(std::vector<int>& vec, std::vector<int>& vec1, std::vector<int>& vec2)
